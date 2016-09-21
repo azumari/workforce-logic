521ea20b6336676968f01aea9edb488c390f0981
Emily Higgins <emilyrchiggins@gmail.com>
2016-09-21 17:58:23 -0400

    Add controller

---
+++ Workforce.Logic.Charlie/Workforce.Logic.Charlie.Rest/Controllers/LocationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Workforce.Logic.Charlie.Rest.Controllers
+{
+    public class LocationController : ApiController
+    {
+
+        public HttpResponseMessage Get(int option)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, option);
+        }
+
+    }
+}
