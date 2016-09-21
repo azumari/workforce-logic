aa76bb6e42331d423e3503932933731e8efae2fa
brian espinosa <career.brian.espinosa@gmail.com>
2016-09-21 15:58:25 -0400

    add HousingComplexDto

---
+++ Workforce.Logic.Grace/Workforce.Logic.Grace.Domain/BusinessModels/Dtos/HousingComplexDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Grace.Domain.BusinessModels.Dtos
+{
+  public class HousingComplexDto
+  {
+    public int HotelID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public bool IsHotel { get; set; }
+    public string PhoneNumber { get; set; }
+    public bool ActiveBit { get; set; }
+  }
+}
