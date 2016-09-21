098fcbe50b10cc9803fd196e9cf39da2c7a0a669
brian espinosa <career.brian.espinosa@gmail.com>
2016-09-21 16:11:29 -0400

    Add domain logic stubs

---
+++ Workforce.Logic.Grace/Workforce.Logic.Grace.Domain/Helpers/LogicHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workforce.Logic.Grace.Domain.BusinessModels.Dtos;
+
+namespace Workforce.Logic.Grace.Domain.Helpers
+{
+  public class LogicHelper
+  {
+
+    public async Task<List<HousingComplexDto>> GetHousingComplexs()
+    {
+      List<HousingComplexDto> hotaps = new List<HousingComplexDto>();
+
+      HousingComplexDto toDelete = new HousingComplexDto()
+      {
+        ActiveBit = false,
+        Address = "123 fake st",
+        HotelID = 1,
+        IsHotel = false,
+        Name = "THIS IS A FAKE NAME",
+        PhoneNumber = "956-793-3185"
+      };
+      hotaps.Add(toDelete);
+      return hotaps;
+    }
+  }
+}
