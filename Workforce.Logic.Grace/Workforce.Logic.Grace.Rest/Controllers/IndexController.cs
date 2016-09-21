a15b4886415d60f73763bd4effef7aacfa6604f4
brian espinosa <career.brian.espinosa@gmail.com>
2016-09-21 19:50:29 -0400

    Add IndexController

---
+++ Workforce.Logic.Grace/Workforce.Logic.Grace.Rest/Controllers/IndexController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Workforce.Logic.Grace.Rest.Controllers
+{
+  public class IndexController : ApiController
+  {
+    public Dictionary<string, string> Get()
+    {
+      var options = new Dictionary<string, string>();
+      options.Add("getAllComplexes", "api/HousingComplex");
+      return options;
+    }
+  }
+}
