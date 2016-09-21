02524c4094d0abb6bed2c68d1f8f07bb8d44a72f
Emily Higgins <emilyrchiggins@gmail.com>
2016-09-21 16:07:42 -0400

    add BusinessModels folder, Helper and Mapper classes

---
+++ Workforce.Logic.Charlie/Workforce.Logic.Charlie.Domain/LogicHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workforce.Logic.Charlie.Domain.BusinessModels;
+
+namespace Workforce.Logic.Charlie.Domain
+{
+    public class LogicHelper
+    {
+
+        public List<LocationDto> GetAllLocations()
+        {
+            var locs = new List<LocationDto>();
+            return locs;
+        }
+
+    }
+}
