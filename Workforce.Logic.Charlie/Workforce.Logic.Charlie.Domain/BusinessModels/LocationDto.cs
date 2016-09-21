9d9767d7c3f029d9450f30a5c7b6145cba5e5125
Emily Higgins <emilyrchiggins@gmail.com>
2016-09-21 16:03:14 -0400

    add BusinessModels folder

---
+++ Workforce.Logic.Charlie/Workforce.Logic.Charlie.Domain/BusinessModels/LocationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Charlie.Domain.BusinessModels
+{
+    public class LocationDto
+    {
+        public int LocationId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
