b7407f752e818d6a1658faee22ae456923ae85eb
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-22 15:33:09 -0400

    adding validation to BatchDto

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/DomainModels/BatchDto.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/DomainModels/BatchDto.cs
@@ -8,10 +8,78 @@ namespace Workforce.Logic.Felice.Domain.DomainModels
 {
    class BatchDto
    {
-      public int BatchID { get; set; }
-      public string Name { get; set; }
-      public int InstrucgtorID { get; set; }
-      public DateTime StartDate { get; set; }
-      public DateTime EndDate { get; set; }
+      private int _BatchID;
+      public int BatchID
+      {
+         get { return _BatchID; }
+         set
+         {
+            if (value > 0)
+            {
+               _BatchID = value;
+            }
+            else throw new ArgumentException("You have entered an invalid integer for BatchID");
+         }
+      }
+
+      private string _Name;
+      public string Name
+      {
+         get { return _Name; }
+         set
+         {
+            if ( !String.IsNullOrWhiteSpace(value))
+            {
+               int count = 0;
+               foreach (char c in value)
+                  count++;
+               if (count <= 50)
+                  _Name = value;
+               else throw new ArgumentException("The batch name cannot be more than 50 characters", "Name");
+            }
+            else
+            {
+               throw new ArgumentException("The batch name is required and cannot be empty");
+            }
+         }
+      }
+
+      private int _InstructorID;
+      public int InstructorID
+      {
+         get { return _InstructorID; }
+         set
+         {
+            if (value > 0)
+            {
+               _InstructorID = value;
+            }
+            else throw new ArgumentException("You have entered an invalid integer for InstructorID");
+         }
+      }
+
+      private DateTime _StartDate;
+      public DateTime StartDate
+      {
+         get { return _StartDate; }
+         set
+         {
+            if (!DateTime.TryParse(value.ToString(), out _StartDate))
+               throw new ArgumentException("The Start Date is invalid");
+            else _StartDate = value;
+         }
+      }
+
+      private DateTime _EndDate;
+      public DateTime EndDate
+      {
+         get { return _EndDate; }
+         set
+         {
+            if (!DateTime.TryParse(value.ToString(), out _EndDate))
+               throw new ArgumentException("The Start Date is invalid");
+            else _StartDate = value;
+         }
+      }
    }
 }
