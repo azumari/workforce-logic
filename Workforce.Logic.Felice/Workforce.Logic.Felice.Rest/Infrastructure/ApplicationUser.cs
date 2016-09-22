0eca773a8e070cdc1b4e31565a8e9503346a1048
wlopez <wlopez.career@gmail.com>
2016-09-22 19:17:36 -0400

    created folder named Infrastructure and a class inside called ApplicationUser

---
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Infrastructure/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workforce.Logic.Felice.Rest.Infrastructure
+{
+  public class ApplicationUser
+  {
+  }
+}
