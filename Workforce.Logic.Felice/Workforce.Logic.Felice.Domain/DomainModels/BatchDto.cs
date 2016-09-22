e0ec3f7369683fdb4e4ce08a3d490b3f12109314
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-22 14:20:12 -0400

    poplulated BatchDto

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/DomainModels/BatchDto.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/DomainModels/BatchDto.cs
@@ -8,6 +8,10 @@ namespace Workforce.Logic.Felice.Domain.DomainModels
 {
    class BatchDto
    {
-
+      public int BatchID { get; set; }
+      public string Name { get; set; }
+      public int InstrucgtorID { get; set; }
+      public DateTime StartDate { get; set; }
+      public DateTime EndDate { get; set; }
    }
 }
