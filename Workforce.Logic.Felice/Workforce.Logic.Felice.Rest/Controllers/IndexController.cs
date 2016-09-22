384cd6feef2f421fdcf403a1497833710a80b733
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-22 10:29:50 -0400

    Index updated to display a menu

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Controllers/IndexController.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Controllers/IndexController.cs
@@ -7,15 +7,17 @@ using System.Web.Http;
 
 namespace Workforce.Logic.Felice.Rest.Controllers
 {
-    public class IndexController : ApiController
-    {
+   public class IndexController : ApiController
+   {
       /// <summary>
       /// This is the base 'Get' method for Index
       /// The primary purpose of Index will be to provide the available "Menu" when an Api call is made to Felice.Rest
       /// </summary>
-      public HttpResponseMessage Get()
+      public Dictionary<string, string> Get()
       {
-         return Request.CreateResponse(HttpStatusCode.OK, "Hello World!! The Index controller works!!");
+         var options = new Dictionary<string, string>();
+         options.Add("getAllComplexes", "api/HousingComplex");
+         return options;
       }
    }
 }
