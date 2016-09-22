13b9b53bd33e2da3a9281137cc3291adf52b847f
tlopez92 <wlopez.career@gmail.com>
2016-09-22 00:11:55 -0400

    Changed the name of the namespace

--- Workforce.Logic.Charlie/Workforce.Logic.Charlie.Rest/App_Start/IdentityConfig.cs
+++ Workforce.Logic.Charlie/Workforce.Logic.Charlie.Rest/App_Start/IdentityConfig.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Workforce.Logic.Charlie.Rest
+namespace Workforce.Logic.Charlie.Rest.App_Start
+{
   public class IdentityConfig
   {
   }
