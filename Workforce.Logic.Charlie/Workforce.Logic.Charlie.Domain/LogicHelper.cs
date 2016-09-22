e532bee32e80ed6ad898a6c5e3ac7f3669a5a17c
Emily Higgins <emilyrchiggins@gmail.com>
2016-09-21 23:49:50 -0400

    Add XML comments to methods

--- Workforce.Logic.Charlie/Workforce.Logic.Charlie.Domain/LogicHelper.cs
+++ Workforce.Logic.Charlie/Workforce.Logic.Charlie.Domain/LogicHelper.cs
@@ -10,9 +10,19 @@ namespace Workforce.Logic.Charlie.Domain
     public class LogicHelper
     {
 
-        public List<LocationDto> GetAllLocations()
+        /// <summary>
+        /// Retreive all active meetup locations.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<LocationDto>> GetAllLocations()
         {
             var locs = new List<LocationDto>();
+            var dummy = new LocationDto();
+            dummy.Name = "Nowhere";
+            dummy.Address = "123 Fictitious Dr, Herndon, VA";
+            dummy.Latitude = 40.5;
+            dummy.Longitude = 40.5;
+            locs.Add(dummy);
             return locs;
         }
 
