d14a503f265fbc500f9c74ac1f9a8f402b2a2da6
wlopez <wlopez.career@gmail.com>
2016-09-22 11:40:17 -0400

    added Test Class to Felice's Logic solution

---
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Tests/EmailTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workforce.Logic.Felice.Tests
+{
+  public class EmailTest
+  {
+  }
+}
