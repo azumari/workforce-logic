f4265185ee656d4790a71b05bac85de5dfc84dd6
tlopez92 <wlopez.career@gmail.com>
2016-09-22 00:18:56 -0400

    Created the email settings for Project Charlie's Logic

--- Workforce.Logic.Charlie/Workforce.Logic.Charlie.Rest/App_Start/IdentityConfig.cs
+++ Workforce.Logic.Charlie/Workforce.Logic.Charlie.Rest/App_Start/IdentityConfig.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace Workforce.Logic.Charlie.Rest.App_Start
 {
-  public class IdentityConfig
+  public class EmailService : IIdentityMessageService
   {
+    public async Task SendAsync(IdentityMessage message)
+    {
+      await configSendEmailasync(message);
+    }
+
+
+    private Task configSendEmailasync(IdentityMessage message)
+    {
+      var email = new MailMessage();
+
+      email.To.Add(message.Destination);
+      email.From = new System.Net.Mail.MailAddress("revature.projectliberate@gmail.com", "Revature");
+      email.Subject = message.Subject;
+      email.Body = message.Body;
+      email.IsBodyHtml = true;
+
+      System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient
+      {
+        Host = ConfigurationManager.AppSettings["GmailHost"],
+        Port = Int32.Parse(ConfigurationManager.AppSettings["GmailPort"]),
+        EnableSsl = true,
+        DeliveryMethod = SmtpDeliveryMethod.Network,
+        UseDefaultCredentials = false,
+        Credentials = new NetworkCredential(ConfigurationManager.AppSettings["GmailUserName"], ConfigurationManager.AppSettings["GmailPassword"])
+
+      };
+      return Task.Run(() => smtp.SendMailAsync(email));
+    }
   }
 }
