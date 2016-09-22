b72f3fd78babff4078b8203ae3a648b8cb4ae239
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-22 11:39:28 -0400

    fixing conflicts

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Controllers/IndexController.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Controllers/IndexController.cs
@@ -4,20 +4,33 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Reflection;
 
 namespace Workforce.Logic.Felice.Rest.Controllers
 {
    public class IndexController : ApiController
    {
       /// <summary>
+      /// collects all methods within a namespace; attempt 1
+      /// </summary>
+      private Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+      {
+         return assembly.GetTypes().Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal)).ToArray();
+      }
+    
+      /// <summary>
       /// This is the base 'Get' method for Index
       /// The primary purpose of Index will be to provide the available "Menu" when an Api call is made to Felice.Rest
       /// </summary>
       public HttpResponseMessage Get()
       {
+         Type[] typelist = GetTypesInNamespace(Assembly.GetExecutingAssembly)
          var options = new Dictionary<string, string>();
 
-         options.Add("Get Associates", "api/associate/");
+         foreach ()
+         {
+            options.Add(, "api/{controller}");
+         }
 
          return Request.CreateResponse(HttpStatusCode.OK, options);
       }
