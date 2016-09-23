cfe78f608f7ecf19bde89d1907779331d8c86537
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-22 21:52:05 -0400

    refactoring associate to accept service reference

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/Associate.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/Associate.cs
@@ -1,29 +1,45 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Workforce.Logic.Felice.Domain.DomainModels;
+using Workforce.Logic.Felice.Domain.WorkforceServiceReference;
 
 namespace Workforce.Logic.Felice.Domain
 {
    public class Associate
    {
+      private readonly FeliceServiceClient client = new FeliceServiceClient();
+
+      
+      private readonly MapperConfiguration associateMapper = new MapperConfiguration(t => t.CreateMap<AssociateDao, AssociateDto>().ForMember(t1 => t1.Gender, opt => opt.MapFrom(t2 => t2.GenderID.ToString())));
+      private readonly MapperConfiguration associateReverseMapper = new MapperConfiguration(t => t.CreateMap<AssociateDto, AssociateDao>().ForMember(t2 => t2.GenderID, m => m.MapFrom(t1 => t1.Gender)));
+      
       /// <summary>
       /// Validates the data coming in from the data layer
       /// </summary>
       public bool ValidateData()
       {
-            return true;
+         return true;
       }
 
       /// <summary>
       /// After successful validation, this method will map the data from the Data Layer to the Dto
       /// </summary>
-      public void MapToDomain()
+      public async Task<List<AssociateDto>> GetAllLocations()
       {
-         
+         var locs = new List<AssociateDto>();
+         var source = await 
+
+         foreach (var item in source)
+         {
+            locs.Add(logic.MapToBusiness(item));
+         }
+         return locs;
       }
+   }
 
       /// <summary>
       /// Validates the data stored in the Dto being passed through from the Client side
@@ -45,9 +61,11 @@ namespace Workforce.Logic.Felice.Domain
       /// <summary>
       /// After validation, this method will Map the data within the Dto to the Data Layer
       /// </summary>
-      public void MapToData()
+      public AssociateDao MapToService(AssociateDto a)
       {
+         var mapper = associateReverseMapper.CreateMapper();
 
+         return mapper.Map<AssociateDao>(a);
       }
    }
 }
