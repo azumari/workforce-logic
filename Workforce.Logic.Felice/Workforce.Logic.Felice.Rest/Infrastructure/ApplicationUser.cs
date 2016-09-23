2dd53695e48d994e77bb8f6fa191a27bdcece8be
wlopez <wlopez.career@gmail.com>
2016-09-23 16:51:37 -0400

    Added new packages and created a few classes and updated a applicationuser

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Infrastructure/ApplicationUser.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Rest/Infrastructure/ApplicationUser.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Workforce.Logic.Felice.Rest.Infrastructure
@@ -26,5 +29,12 @@ namespace Workforce.Logic.Felice.Rest.Infrastructure
 
     [Required]
     public DateTime JoinDate { get; set; }
+
+    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+    {
+      var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+
+      return userIdentity;
+    }
   }
 }
