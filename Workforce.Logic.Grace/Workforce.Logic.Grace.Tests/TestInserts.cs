e8d283d0718ca111abde0465f925eb47ee9b7931
brian espinosa <career.brian.espinosa@gmail.com>
2016-09-25 14:36:13 -0400

    Add test insert Apartment

---
+++ Workforce.Logic.Grace/Workforce.Logic.Grace.Tests/TestInserts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workforce.Logic.Grace.Domain.BusinessModels.Dtos;
+using Workforce.Logic.Grace.Domain.Helpers;
+using Xunit;
+
+namespace Workforce.Logic.Grace.Tests
+{
+  public class TestInserts
+  {
+
+    private readonly LogicHelper logicHelper = new LogicHelper();
+ 
+    
+
+    [Fact]
+    public async Task Test_InsertApartment()
+    {
+      
+      ApartmentDto aptDto = new ApartmentDto()
+      {
+        CurrentCapacity = 2,
+        GenderID = null,
+        HotelID = null,
+        MaxCapacity = 10,
+        RoomNumber = 8460
+      };
+
+      bool passed = await logicHelper.AddApartment(aptDto);
+
+      Assert.True(passed);
+    }
+
+  }
+}
