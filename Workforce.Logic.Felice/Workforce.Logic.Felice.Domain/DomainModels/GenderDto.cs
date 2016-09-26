84a264ee26acc0aa429d5d1abc460b451b041499
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-26 12:08:15 -0400

    added annotation for GenderDto

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/DomainModels/GenderDto.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/DomainModels/GenderDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Workforce.Logic.Felice.Domain.DomainModels
    public class GenderDto
    {
       public int GenderID { get; set; }
+      [Required]
       public string Name { get; set; }
    }
 }
