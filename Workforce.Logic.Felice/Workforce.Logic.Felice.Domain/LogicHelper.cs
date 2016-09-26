fb5aa934b2d5646f3b74615691e28146925a0438
brandon.parnoff <brandon.parnoff@outlook.com>
2016-09-25 20:10:56 -0400

    added AddressDto; added Address; Added new 'get' methods to LogicHelper

--- Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/LogicHelper.cs
+++ Workforce.Logic.Felice/Workforce.Logic.Felice.Domain/LogicHelper.cs
@@ -12,20 +12,64 @@ namespace Workforce.Logic.Felice.Domain
    {
       private readonly FeliceServiceClient client = new FeliceServiceClient();
       private readonly Associate associateLogic = new Associate();
+      private readonly Address addressLogic = new Address();
+      private readonly Batch batchLogic = new Batch();
+      //private readonly Gender genderLogic = new Gender();
+      private readonly Instructor instructorLogic = new Instructor();
 
       public async Task<List<AssociateDto>> GetAllAssociates()
       {
          var associate = new List<AssociateDto>();
          var serviceAssociates = await client.GetAssociatesAsync();
+         var serviceGenders = await client.GetGenderAsync();
 
          foreach (var item in serviceAssociates)
          {
             if (associateLogic.ValidateSoapData(item))
             {
-               associate.Add(associateLogic.MapToRest(item));
+               var parse = associateLogic.MapToRest(item);
+               parse.Gender = serviceGenders.FirstOrDefault(g => g.GenderID.Equals(item.GenderID)).Name;
+
+               associate.Add(parse);
             }
          }
          return associate;
+      }
+      /*
+      public AssociateDto AddNewAssociate()
+      {
+         var associate = new AssociateDto();
+         return ;
+      }*/
+
+      public async Task<List<AddressDto>> GetAllAddresses()
+      {
+         var address = new List<AddressDto>();
+         var serviceAddress = await client.GetAddressAsync();
+
+         foreach (var item in serviceAddress)
+         {
+            if (addressLogic.ValidateSoapData(item))
+            {
+               address.Add(addressLogic.MapToRest(item));
+            }
+         }
+         return address;
+      }
+
+      public async Task<List<BatchDto>> GetAllBatches()
+      {
+         var batches = new List<BatchDto>();
+         var serviceBatches = await client.GetBatchesAsync();
+
+         foreach (var item in serviceBatches)
+         {
+            if (batchLogic.ValidateSoapData(item))
+            {
+               batches.Add(batchLogic.MapToRest(item));
+            }
+         }
+         return batches;
       }
    }
 }
