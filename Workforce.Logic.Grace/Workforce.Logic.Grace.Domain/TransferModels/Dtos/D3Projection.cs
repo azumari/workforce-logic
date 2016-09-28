0aec2b6f44994d1fe8aa23ddaf38a8c73930b16d
brian espinosa <career.brian.espinosa@gmail.com>
2016-09-28 05:00:25 -0400

    tweak logic for projection

--- Workforce.Logic.Grace/Workforce.Logic.Grace.Domain/TransferModels/Dtos/D3Projection.cs
+++ Workforce.Logic.Grace/Workforce.Logic.Grace.Domain/TransferModels/Dtos/D3Projection.cs
@@ -9,13 +9,8 @@ namespace Workforce.Logic.Grace.Domain.TransferModels.Dtos
   public class D3Projection
   {
     //stuff from Batch
-    public string Name { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public int count { get; set; }
-    //stuff from both TRAINEE and BATCH
-    public int BatchID { get; set; }
-
-    public DateTime TestDate { get; set; }
+    public String Date{ get; set; }
+    public int CurrentCapacity { get; set; }
+    public int TotalMax { get; set; }
   }
 }
